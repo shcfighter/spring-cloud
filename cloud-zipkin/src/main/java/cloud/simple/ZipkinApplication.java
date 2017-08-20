30679b371e82586345a13bd5f9b04f364b76a513
za-wangshenhua <za-wangshenhua@zaonline.com>
2017-08-20 15:08:18 +0800

    config bus

--- cloud-zipkin/src/main/java/cloud/simple/ZipkinApplication.java
+++ cloud-zipkin/src/main/java/cloud/simple/ZipkinApplication.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
  * @author shc_fighter
  */
 @EnableZipkinStreamServer
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 public class ZipkinApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApplication.class, args);
